833083b1699b30b33f3b8ed0e82c0fb252a8894d
Yuri Pinheiro <yuripinheirot@gmail.com>
2020-11-30 10:51:02 -0400

    Finalizado

---
+++ semana3/variaveis/js-vanilla-template/index.js
@@ -0,0 +1,71 @@
+/*
+a = 10
+b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b)
+
+//10
+//10 5
+
+
+a = 10
+b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c)
+
+// 10 10 10
+
+*/
+
+
+// let nome;
+// let idade;
+
+// console.log(typeof(nome));
+// console.log(typeof(idade));
+// // apareceu undefined por que a variavei nao tem valor atribuido
+
+// nome = prompt("Qual é seu nome?");
+// idade = prompt("Qual é sua idade?");
+
+// console.log(nome, idade);
+// // notei que as variaveis imprimiram e os valores solicitados foram armazenado nelas
+
+// console.log(`Olá ${nome}! voce tem ${idade} anos.`); 
+
+// let nome;
+// let profissao;
+// let cidade;
+// let idade;
+// let escola;
+
+// nome = prompt("Qual é seu nome?");
+// profissao = prompt("Qual é sua profissao?");
+// cidade = prompt("Em qual cidade voce mora?");
+// idade = prompt("Qual é sua idade?");
+// escola = prompt("Aonde voce estuda?");
+
+// console.log(`Olá ${nome}! voce tem ${idade} anos, mora em ${cidade}, 
+// é ${profissao} e estuda na ${escola}.`); 
+
+// let foods = ['pizza','hamburguer','picanha','lasanha','fricasse'];
+
+// console.log(foods);
+// console.log(`Essas sao minhas comidas preferidas: ${foods[0]}, ${foods[1]}, 
+// ${foods[2]}, ${foods[3]}, ${foods[4]}.`);
+
+// foods[1] = prompt(`Qual a sua comida preferida?`);
+// console.log(foods);
+
+let questions = [`Voce ja tomou banho hoje?`, `Voce e loiro?`, `Gosta de esportes?`];
+let answers = [true,false,true];
+
+console.log(questions[0], answers[0]);
+console.log(questions[1], answers[1]);
+console.log(questions[2], answers[2]);
