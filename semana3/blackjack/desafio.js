2d61010d243f19cb8d5dcbcad21a3fe25b23ed8b
Yuri Pinheiro <yuripinheirot@gmail.com>
2020-12-04 14:35:49 -0400

    Comentado codigo

--- semana3/blackjack/desafio.js
+++ semana3/blackjack/desafio.js
@@ -14,6 +14,7 @@
 console.log(`Bem vindo ao jogo de Blackjack!`);
 if (confirm(`Quer iniciar uma nova rodada?`)) {
 
+   //players
    let jogador1 = {
       cartas: [],
       stringCartas: '',
@@ -24,14 +25,16 @@ if (confirm(`Quer iniciar uma nova rodada?`)) {
       stringCartas: '',
       pontuacao: 0
    }
+   //msg final
    let resultado = '';
 
-
+   //compra inicial
    for (let index = 1; index <= 2; index++) {
       jogador1.cartas.push(comprarCarta())
       jogador2.cartas.push(comprarCarta())
    }
 
+   //enquanto um dos dois ou os dois sairem com ases na mao, será sorteado novamente
    while (String(jogador1.cartas[0].texto).substring(0, 1) + String(jogador1.cartas[1].texto).substring(0, 1) === 'AA' ||
       String(jogador2.cartas[0].texto).substring(0, 1) + String(jogador1.cartas[1].texto).substring(0, 1) === 'AA') {
       for (let index = 1; index <= 2; index++) {
@@ -40,6 +43,7 @@ if (confirm(`Quer iniciar uma nova rodada?`)) {
       }
    }
 
+   //calculos de pontuação e criação da string de cartas
    for (const valor of jogador1.cartas) {
       jogador1.pontuacao += valor.valor;
       jogador1.stringCartas += valor.texto + ' ';
@@ -48,7 +52,8 @@ if (confirm(`Quer iniciar uma nova rodada?`)) {
       jogador2.pontuacao += valor.valor;
       jogador2.stringCartas += valor.texto + ' ';
    }
-   //jogador 1
+
+   //vez do jogador 1
    while (jogador1.pontuacao < 21 && confirm(`Suas cartas são ${jogador1.stringCartas}. A carta revelada do jogador 2 é ${jogador2.stringCartas.substring(0, 3)}.\n`
       + `Deseja comprar mais uma carta?`)) {
       jogador1.cartas.push(comprarCarta());
@@ -56,7 +61,7 @@ if (confirm(`Quer iniciar uma nova rodada?`)) {
       jogador1.stringCartas += jogador1.cartas[jogador1.cartas.length - 1].texto + ' ';
    }
 
-   //jogador 2
+   //vez do jogador 2
    while (jogador1.pontuacao < 21 && confirm(`Suas cartas são ${jogador2.stringCartas}. A carta revelada do jogador 1 é ${jogador1.stringCartas.substring(0, 3)}.\n`
       + `Deseja comprar mais uma carta?`)) {
       jogador2.cartas.push(comprarCarta());
@@ -64,6 +69,7 @@ if (confirm(`Quer iniciar uma nova rodada?`)) {
       jogador2.stringCartas += jogador2.cartas[jogador2.cartas.length - 1].texto + ' ';
    }
 
+   //apuração dos resultados
    if (jogador1.pontuacao > 21 && jogador2.pontuacao <= 21) {
       resultado = `O jogador 2 ganhou! `
    }
@@ -80,11 +86,10 @@ if (confirm(`Quer iniciar uma nova rodada?`)) {
       resultado = `O jogo empatou!`;
    }
 
+   //resultado
    prompt(`O jogador 1 saiu com as cartas: ${jogador1.stringCartas} e sua pontuação é: ${jogador1.pontuacao}\n` +
       `O jogador 2 saiu com as cartas: ${jogador2.stringCartas} e sua pontuação é: ${jogador2.pontuacao}\n` +
       resultado);
-
-
 } else {
    console.log(`O jogo acabou!`);
 }
