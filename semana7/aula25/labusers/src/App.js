ce32586007a6cf837cdf26108fd8eaf44eee2b4b
Yuri Pinheiro <yuripinheirot@gmail.com>
2021-01-12 13:54:52 -0400

    Feito

---
+++ semana7/aula25/labusers/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import CriarUsuario from './components/criarUsuario'
+import ListaUsuarios from './components/listaUsuario'
+
+function App() {
+  return (
+    <div className="App">
+      <CriarUsuario />
+      <ListaUsuarios />
+    </div>
+  );
+}
+
+export default App;
