e6d37e1831a2c7dc4ef543e9a3994a6fc085886d
Yuri Pinheiro <yuripinheirot@gmail.com>
2021-01-15 12:30:34 -0400

    Nada feito

---
+++ semana7/aula27/wikibands/src/components/BandCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class Main extends React.Component {
+
+  
+  render() {
+    return (
+      <div>
+        <h2>Iron Maiden</h2>
+      </div>
+    )
+  }
+}
