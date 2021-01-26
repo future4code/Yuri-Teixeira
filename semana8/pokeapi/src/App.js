14a1f2af4ee7ed3640ce048e23dc9f9ffc25c1fd
Yuri Pinheiro <yuripinheirot@gmail.com>
2021-01-26 16:56:28 -0400

    Terminando exercicio

--- semana8/pokeapi/src/App.js
+++ semana8/pokeapi/src/App.js
@@ -12,8 +12,8 @@ export default function App() {
   //   pokeName: "",
   // };
 
-  const { pokeList, setPokeList } = useState([]);
-  const { pokeName, setPokeName } = useState("");
+  let [ pokeList, setPokeList ] = useState([]);
+  let [ pokeName, setPokeName ] = useState("");
 
   // método que roda após a montagem do componente
   // componentDidMount = () => {
@@ -34,7 +34,7 @@ export default function App() {
       .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
       .then((response) => {
         // função que está setando no estado os 151 pokemons
-        this.setState({ pokeList: response.data.results });
+        setPokeList( pokeList = response.data.results );
       })
       .catch((err) => {
         console.log(err);
@@ -42,17 +42,17 @@ export default function App() {
   });
 
   const changePokeName = (event) => {
-    this.setState({ pokeName: event.target.value });
+    setPokeName( pokeName = event.target.value );
   };
 
   return (
     <div className="App">
       {/* evento onChange chama função toda vez que o usuário 
         escolhe um novo pokemon no dropdown */}
-      <select onChange={this.changePokeName}>
+      <select onChange={changePokeName}>
         <option value={""}>Nenhum</option>
         {/* renderizando a lista de pokemons como opções do select */}
-        {this.state.pokeList.map((pokemon) => {
+        {pokeList.map((pokemon) => {
           return (
             <option key={pokemon.name} value={pokemon.name}>
               {pokemon.name}
@@ -62,7 +62,7 @@ export default function App() {
       </select>
       {/* expressão booleana que renderiza o componente PokeCard,
         caso o valor de pokeName, no estado, seja true */}
-      {this.state.pokeName && <PokeCard pokemon={this.state.pokeName} />}
+      {pokeName && <PokeCard pokemon={pokeName} />}
     </div>
   );
 }
