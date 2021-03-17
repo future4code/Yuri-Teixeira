27596abd22803dce44c5aa434882997922a35aad
Yuri Pinheiro <yuripinheirot@gmail.com>
2021-03-16 21:39:14 -0400

    Terminando por aqui, flw

--- semana15/aula21/exercicio/src/exercicio8.ts
+++ semana15/aula21/exercicio/src/exercicio8.ts
@@ -1,11 +1,24 @@
-let pratos = require('./exercicio9.json');
+const pratos = require("./exercicio8.json");
+import fs from "fs";
 
 type prato = {
   nome: string;
   custo: number;
   valor: number;
-  ingredientes: [];
+  ingredientes: string[];
 };
 
+const novoPrato: prato = {
+  nome: "macarronada2",
+  custo: 5,
+  valor: 10,
+  ingredientes: ["macarrao", "molho", "queijo"],
+};
 
+const addPrato = (prato: prato): void => {
+  pratos.push(prato);
+  fs.writeFileSync("./exercicio8.json", JSON.stringify(pratos));
+  console.log(pratos);
+};
 
+addPrato(novoPrato);
