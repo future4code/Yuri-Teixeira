8fbedbfd24cb9d2b4b7bd034ada286c403970c99
Yuri Pinheiro <yuripinheirot@gmail.com>
2021-03-26 23:25:28 -0400

    Terminado exercicios

--- semana16/toDoList/src/index.ts
+++ semana16/toDoList/src/index.ts
@@ -1,6 +1,13 @@
 import app from "./app";
-import express, { Request, Response } from "express";
-import { createUser, getUserById } from "./reqFunctions";
+import { Request, Response } from "express";
+import moment from "moment";
+import {
+  createUser,
+  getUserById,
+  editUser,
+  createTask,
+  getTasks,
+} from "./reqFunctions";
 
 //CREATE USER
 app.put("/user", async (req: Request, res: Response) => {
@@ -33,9 +40,9 @@ app.get(
       if (isNaN(id)) {
         throw new Error("Invalid id!");
       }
-      
+
       const result = await getUserById(id.toString());
-      
+
       if (result.length === 0) {
         errorCode = 404;
         throw new Error("User not found!");
@@ -47,3 +54,85 @@ app.get(
     }
   }
 );
+
+// EDIT USER
+app.post(
+  "/user/edit/:id",
+  async (req: Request, res: Response): Promise<any> => {
+    let errorCode = 400;
+    const id: number = Number(req.params.id);
+    const name: string = req.body.name;
+    const nickname: string = req.body.nickname;
+    const email: string = req.body.email;
+    const getUser = await getUserById(id.toString());
+
+    try {
+      if (isNaN(id)) {
+        throw new Error("Invalid id!");
+      }
+      if (name === "" || nickname === "" || email === "") {
+        throw new Error("Invalid body!");
+      }
+      if (getUser.length === 0) {
+        errorCode = 404;
+        throw new Error("User not found!");
+      }
+
+      await editUser(id.toString(), name, nickname, email);
+      res.status(200).send({ message: "User successfully changed." });
+    } catch (error) {
+      res.status(errorCode).send(error.message);
+    }
+  }
+);
+
+// CREATE TAKS
+app.put(
+  "/task",
+  async (req: Request, res: Response): Promise<any> => {
+    let errorCode = 400;
+    const title: string = req.body.title as string;
+    const description: string = req.body.description as string;
+    const limitDate: string = req.body.limitDate as string;
+    const status: string = req.body.status as string;
+    const id_createdby: number = Number(req.body.id_createdby);
+
+    try {
+      if (!title || !description || !limitDate || !status || !id_createdby) {
+        throw new Error("Invalid body!");
+      }
+
+      const result = await createTask(
+        title,
+        description,
+        moment(limitDate, "DD/MM/YYYY").format("YYYY-MM-DD"),
+        status,
+        id_createdby.toString()
+      );
+      
+      res.status(200).send(result);
+    } catch (error) {
+      res.status(errorCode).send(error.message);
+    }
+  }
+);
+
+// GET TASKS
+app.get("/tasks/", async (req: Request, res: Response) => {
+  let errorCode = 400;
+  try {
+    const result = await getTasks();
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(errorCode).send(error.message);
+  }
+});
+app.get("/tasks/:id", async (req: Request, res: Response) => {
+  let errorCode = 400;
+  try {
+    const result = await getTasks(req.params.id);
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(errorCode).send(error.message);
+  }
+});
--- semana16/toDoList/src/reqFunctions.ts
+++ semana16/toDoList/src/reqFunctions.ts
@@ -11,5 +11,48 @@ export const createUser = async (
 };
 
 export const getUserById = async (id: string): Promise<any> => {
-  return connection.select("id", "nickname").from(`users`).where(`id`, id);;
+  return await connection
+    .select("id", "nickname")
+    .from(`users`)
+    .where(`id`, id);
+};
+
+export const editUser = async (
+  id: string,
+  name: string,
+  nickname: string,
+  email: string
+): Promise<any> => {
+  await connection(`users`)
+    .update({
+      name: name,
+      nickname: nickname,
+      email: email,
+    })
+    .where({ id: id });
+};
+
+export const createTask = async (
+  title: string,
+  description: string,
+  limitDate: string,
+  status: string,
+  id_createdby: string
+): Promise<any> => {
+  return await connection("tasks").insert({
+    title: title,
+    description: description,
+    limitDate: limitDate,
+    status: status,
+    id_createdby: id_createdby,
+  });
+};
+
+export const getTasks = async (id?: string): Promise<any> => {
+  if(!id){
+    return  await connection(`tasks`);
+  } else {
+    return  await connection(`tasks`).where({ id: id });
+  }
+ 
 };
