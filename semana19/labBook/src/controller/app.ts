6500631c678ef9c0b382ad5b5b7e895b769e699e
Yuri Pinheiro <yuripinheirot@gmail.com>
2021-04-13 21:40:06 -0400

    Começando organizar endpoints

--- semana19/labBook/src/controller/app.ts
+++ semana19/labBook/src/controller/app.ts
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(cors());
 
 app.listen(3003, () => {
-  console.log("Server running on port 3003")
-})
+  console.log("Server running on port 3003");
+});
 
 export default app;
