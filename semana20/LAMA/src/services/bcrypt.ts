1a49141080cff31778d6423a25c7ce10afe6540e
Yuri Pinheiro <yuripinheirot@gmail.com>
2021-04-24 12:05:26 -0400

    Instalando dependencias

---
+++ semana20/LAMA/src/services/bcrypt.ts
@@ -0,0 +1,16 @@
+import * as bcryptjs from "bcryptjs";
+
+export default class bcrypt {
+   hash = async (plainText: string): Promise<string> => {
+    const rounds = Number(process.env.BCRYPT_COST);
+    const salt = await bcryptjs.genSalt(rounds);
+    return bcryptjs.hash(plainText, salt);
+  };
+
+   compare = async (
+    plainText: string,
+    cypherText: string
+  ): Promise<boolean> => {
+    return bcryptjs.compare(plainText, cypherText);
+  };
+}
