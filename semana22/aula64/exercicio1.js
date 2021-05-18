378668b2740d96124402567ada5f1fddf8ccb55a
Yuri Pinheiro <yuripinheirot@gmail.com>
2021-05-17 22:22:52 -0400

    ok

---
+++ semana22/aula64/exercicio1.js
@@ -0,0 +1,17 @@
+const execute = (strA, strB) => {
+  if (Math.abs(strA.length - strB.length) > 1) {
+    return false;
+  }
+
+  if (strA.length > strB.length) return strA.includes(strB);
+  if (strB.length > strA.length) return strB.includes(strA);
+
+  let charsDiffCount = 0;
+  for (let i = 0; i < strA.length; i++) {
+    if (strA[i] !== strB[i]) charsDiffCount++;
+  }
+
+  return charsDiffCount === 1;
+};
+
+console.log(execute("banana", "banan"));
